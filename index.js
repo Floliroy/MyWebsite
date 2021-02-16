3dac85cb18a845723fa0085e4c969a514b27077f
Flo Leeroy <florian.leeroy@hotmail.com>
2021-02-17 00:59:00 +0100

    redirection http vers https

--- index.js
+++ index.js
@@ -100,6 +100,9 @@ app.get("/switchLang", function(req, res){
 /**
  * Start server
  */
+http.get('*', function(req, res) { 
+    res.redirect('https://' + req.headers.host + req.url);
+})
 app.use(express.static("public"))
 app.use(function (req, res){
 	res.status(404)
