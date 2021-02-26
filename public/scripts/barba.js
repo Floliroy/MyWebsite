c3c84b4e072a6964547c6cf125cf8bca41f77774
Flo Leeroy <florian.leeroy@hotmail.com>
2021-02-26 20:22:10 +0100

    with reverse image transition

--- public/scripts/barba.js
+++ public/scripts/barba.js
@@ -3,10 +3,10 @@
  */
 let loader = $(".loader")
 //Function at beginning of page change
-function loaderIn() {
+function loaderIn(namespace) {
     return gsap.fromTo(loader,
         {
-            rotation: -30,
+            rotation: namespace == "home" ? 30 : -30,
             scaleX: 0,
             xPercent: -5
         },
@@ -15,19 +15,19 @@ function loaderIn() {
             xPercent: 0,
             scaleX: 1,
             rotation: 0,
-            ease: 'power4.inOut',
-            transformOrigin: 'left center'
+            ease: "power4.inOut",
+            transformOrigin: `${namespace == "home" ? "right" : "left"} center`
         })
 }
 //Function at end of page change
-function loaderAway() {
+function loaderAway(namespace) {
     return gsap.to(loader, {
         duration: 0.8,
         scaleX: 0,
-        xPercent: 5,
-        rotation: 30,
-        transformOrigin: 'right center',
-        ease: 'power4.inOut'
+        xPercent: namespace == "home" ? -5 : 5,
+        rotation: namespace == "home" ? -30 : 30,
+        transformOrigin: `${namespace == "home" ? "left" : "right"} center`,
+        ease: "power4.inOut"
     })
 }
 //Init of page change div animation
@@ -36,32 +36,34 @@ gsap.set(loader, {
     rotation: -30,
     xPercent: -5,
     yPercent: 50,
-    transformOrigin: 'left center',
+    transformOrigin: "left center",
     autoAlpha: 1
 })
 barba.hooks.enter(function(){
     window.scrollTo(window.scrollX, 0)
 })
-//Recreate twitch embed player
-barba.hooks.after(function(){
-    if($("#twitch-embed").length){
-        const width = document.querySelector("#main").offsetWidth - 46
-        new Twitch.Embed("twitch-embed", {
-            width: "100%",
-            height: (width-340)/(16/9),
-            channel: "tacticalmonkeyesport",
-            parent: ["tms.floliroy.fr"],
-        })
-    }
-})
 //Barba init
 barba.init({
     transitions: [{
-        async leave() {
-            await loaderIn()
+        name: "default",
+        async leave() {            
+            $(".loader").css("background", `url("../images/transition${$("body").hasClass("blackTheme")?"Black":"White"}.png")`)
+            await loaderIn("default")
         },
         enter() {
-            loaderAway()
+            loaderAway("default")
+        }
+    },{
+        name: "home",
+        to: {
+            namespace: ["index"]
+        },
+        async leave() {
+            $(".loader").css("background", `url("../images/transition${$("body").hasClass("blackTheme")?"Black":"White"}Reverse.png")`)
+            await loaderIn("home")
+        },
+        enter() {
+            loaderAway("home")
         }
     }]
 })
